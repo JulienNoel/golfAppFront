50e056f9a27504c0c70af1c877466af66c06af1d
Alexis Melia <39966501+Alexismla@users.noreply.github.com>
2022-05-16 18:09:36 +0200

    Update Marker Golf + InitialRegion = userLocation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,16 +8,15 @@ import {
 } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
-import Icon from "react-native-vector-icons/FontAwesome";
-
 export default function MapScreen() {
   const [location, setLocation] = useState({});
+  const [locationInit, setLocationInit] = useState({});
 
   const [pseudo, setPseudo] = useState("");
 
@@ -25,7 +24,13 @@ export default function MapScreen() {
     async function askPermissions() {
       var { status } = await Permissions.askAsync(Permissions.LOCATION);
       if (status === "granted") {
-        Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+        var locationTemp = await Location.getCurrentPositionAsync();
+        console.log("INIT", locationTemp);
+        setLocationInit({
+          longitude: locationTemp.coords.longitude,
+          latitude: locationTemp.coords.latitude,
+        });
+        Location.watchPositionAsync({ distanceInterval: 2 }, (location) => {
           setLocation({
             longitude: location.coords.longitude,
             latitude: location.coords.latitude,
@@ -34,7 +39,9 @@ export default function MapScreen() {
       }
     }
     askPermissions();
-  }, [location]);
+  }, []);
+
+  console.log("okok", locationInit);
 
   let listGolf = [
     {
@@ -62,23 +69,11 @@ export default function MapScreen() {
   var golf = [
     {
       name: "golf1",
-      coordinate: { latitude: 48.86777683776753, latitude: 2.303689483736294 },
+      coordinate: { latitude: 48.86777683776753, longitude: 2.303689483736294 },
     },
     {
       name: "golf2",
-      coordinate: { latitude: 48.84777683776753, latitude: 2.303689483736294 },
-    },
-    {
-      name: "golf3",
-      coordinate: { latitude: 48.83777683776753, latitude: 2.303689483736294 },
-    },
-    {
-      name: "golf4",
-      coordinate: { latitude: 48.82777683776753, latitude: 2.303689483736294 },
-    },
-    {
-      name: "golf5",
-      coordinate: { latitude: 48.88777683776753, latitude: 2.303689483736294 },
+      coordinate: { latitude: 40, longitude: 4 },
     },
   ];
 
@@ -108,32 +103,43 @@ export default function MapScreen() {
   var markerDisplayGolf = golf.map((point, i) => (
     <Marker
       key={i}
-      coordinate={point.coordinate}
+      coordinate={{
+        latitude: point.coordinate.latitude,
+        longitude: point.coordinate.longitude,
+      }}
       title={point.name}
-      image={require("../assets/GolfMarker.jpg")}
-    />
+    >
+      <Image source={require("../assets/GolfMarker.png")} />
+    </Marker>
   ));
 
-  return (
-    <MapView
-      style={{ flex: 1 }}
-      region={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    >
-      <Marker
-        image={require("../assets/UserMarker.jpg")}
-        coordinate={{
-          latitude: location.latitude,
-          longitude: location.longitude,
+  console.log(location);
+  if (locationInit.latitude) {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: locationInit.latitude,
+          longitude: locationInit.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-      />
-      {markerDisplayGolf}
-    </MapView>
-  );
+      >
+        <Marker
+          image={require("../assets/UserMarker.png")}
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          style={{ width: 26, height: 28 }}
+          resizeMode="contain"
+        />
+        {markerDisplayGolf}
+      </MapView>
+    );
+  } else {
+    return <Text>Charg</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
