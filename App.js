ed3ede4ded6fd73aaa3122ad5db24dbd7032a214
Julien Noel <jncoweb@gmail.com>
2022-05-16 11:24:22 +0200

    modif pas importante

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen'
+import ScoreScreen from './screens/ScoreScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function BottomNavigator () {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          let iconName;
+
+          if (route.name == 'Chat') {
+            iconName = 'chatbubble';
+          }else if (route.name == 'Home') {
+            iconName = 'md-home-sharp';
+          }
+  
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+        })}
+      tabBarOptions={{
+        activeTintColor: '#eb4d4b',
+        inactiveTintColor: '#FFFFFF',
+        style: {
+          backgroundColor: '#e67e22',
+        }
+      }}
+    >
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Score" component={ScoreScreen} />
+    </Tab.Navigator>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  return (
+    
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
+  );
+}
+
