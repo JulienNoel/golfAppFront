7bcf07a20905d5c0acee822a1ca2c6741ad5022b
Edouard Roussel <edouardroussel@Edouards-MacBook-Pro.local>
2022-05-20 00:15:23 +0200

    score parcours avec réservation

--- screens/Score/ScorePageStart.js
+++ screens/Score/ScorePageStart.js
@@ -45,6 +45,15 @@ const styles = StyleSheet.create({
     height: '12%',
     borderRadius: 5,
     margin: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
   image: {
     flex: 1,
