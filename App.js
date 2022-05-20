3402cde53ddc3a404b7a4ce2e64c7bef4ec8932c
Julien Noel <jncoweb@gmail.com>
2022-05-20 15:00:58 +0200

    maj screen app

--- App.js
+++ App.js
@@ -99,7 +99,7 @@ function BottomNavigator() {
       }}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="StackMap" component={LogScreen} />
+      <Tab.Screen name="StackMap" component={StackMapScreen} />
       <Tab.Screen name="Score" component={StackScoreScreen} />
     </Tab.Navigator>
   );
