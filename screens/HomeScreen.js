c8fac1b651da932d381b7e54cbd8b4d6cf14c0c7
ShaShaShaaaady <cmalherbe@systra.com>
2022-05-23 09:15:44 +0200

    Shady: pour R

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,8 @@ function HomeScreen(props) {
     }
     GolfFromBdd();
   }, []);
+
+  
   return (
     <View style={styles.container}>
       <View style={{ flexDirection: "row", alignItems: "center", justifyContent:"space-between", height:"12%", width:"100%", marginTop:50}}>
